dbcc97cbfb7dc19f3b7fb52447477a8e5dedec46
MuriloEC <murilocasagrande71@gmail.com>
2026-05-07 21:54:04 -0300

    delete de pasta

--- api/calculator.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from services.calculator import calcular_valor
-
-app=FastAPI()
-
-@app.get("/api/calcular")
-def calcular(a: float, b: float, c:float, HP: int):
-    resultado = calcular_valor(a, b, c, HP)
-    return {"resultado": resultado}
