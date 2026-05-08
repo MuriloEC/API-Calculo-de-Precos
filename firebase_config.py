3e033fe6c5e413ef9c59a2d7bb962c645cb6c3b0
MuriloEC <murilocasagrande71@gmail.com>
2026-05-07 21:50:30 -0300

    backup

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,9 @@
 import firebase_admin
+import json
 from firebase_admin import credentials, firestore
 
 # Esconder a chave antes de hospedar
-cred = credentials.Certificate("SUA_CREDENCIAL_AQUI.json")
+cred = credentials.Certificate("SUA_CHAVE.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
-
